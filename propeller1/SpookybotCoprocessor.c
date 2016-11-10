fe6c0e6e16c90bd2d021e3d9445e743ac6232f0b
Jonathan Johnson <jonathanjohnson@iheartmedia.com>
2016-11-10 17:09:09 -0500

    added application struct

--- propeller1/SpookybotCoprocessor.c
+++ propeller1/SpookybotCoprocessor.c
@@ -10,15 +10,50 @@
   
   Parallax propeller has 8 cores. 
   6 cores will run sensor detect and poll data (1 -5)
-  1 core will run batch writing data to eeprom and detecting outside eeprom read (0)
+  1 core will run batch writing data to eeprom (0)
   1 core will communicate with host over i2c (6)
 */
 #include "simpletools.h"                     
-
+#include "TemperatureModule.h"
 #define COG_MIN_MEMORY 40
 #define SENSOR_DETECT_STACK_SIZE 60
-int main() {
+
+
+typedef struct ApplicationArguments {
+  void (*module1)(struct cogInit *init);
+  void (*module2)(struct cogInit *init);
+  void (*module3)(struct cogInit *init);
+  void (*module4)(struct cogInit *init);
+  void (*module5)(struct cogInit *init);
+  void (*module6)(struct cogInit *init);
+} Application;
+
+
+void loadModules(){
   
+}  
+
+void waitOneSecond(){
+  waitcnt(CNT + CLKFREQ);
+}  
+
+void writeToEeprom(){
+  
+}  
+
+int isShuttingDown(){
+  return 0;
+}  
+
+int main() {
+  loadModules();
+  while (1){
+    if (isShuttingDown()){
+      break; 
+    }      
+    waitOneSecond();
+    writeToEeprom();
+  }    
   return 0;    
 }
 
---
+++ propeller1/TemperatureModule.h
@@ -0,0 +1,7 @@
+#ifndef TEMPERATURE_MODULE_H
+
+#define TEMPERATURE_MODULE_H
+
+void temperatureModuleMain(struct cogInit *init);
+
+#endif
